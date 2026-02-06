81cfda0510641ceec2e425c0ca8528a7e83a1f8a
Muheirwe Oyebare Gillian <muheirwegillian@gmail.com>
2026-02-06 11:52:58 -0800

    final commit of calculator.

---
+++ script.js
@@ -0,0 +1,27 @@
+let inputBox = document.getElementById('inputBox');
+let button = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(button);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            inputBox.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            inputBox.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length - 1);
+            inputBox.value = string;
+        }
+
+        else {
+        string += e.target.innerHTML;
+        input.value = string;
+        }
+
+    });
+});
